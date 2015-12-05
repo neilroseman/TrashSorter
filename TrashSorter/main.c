661ac3be512779c0891dffd2d635c9f0e205260e
Neil Roseman <neil@gojotter.com>
2015-12-05 06:39:52 -0800

    Initial Commit

---
+++ TrashSorter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TrashSorter
+//
+//  Created by NEIL ROSEMAN on 12/5/15.
+//  Copyright (c) 2015 NEIL ROSEMAN. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
